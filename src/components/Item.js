738d28798e5f4859eb7ad7387e8d734c1897eaf1
DonnieAndMooie <owenjeaves@gmail.com>
2023-03-04 11:08:27 +0000

    Create item and display them in grid

---
+++ src/components/Item.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+
+export default function Item({title, imgSrc, price}) {
+  return (
+    <div className='item'>
+        <h3 className='item-heading'>{title}</h3>
+        <img className='item-img' src={imgSrc} alt={title} />
+        <p className='item-price'>{price}</p>
+        <Link className="item-link">View</Link>
+    </div>
+  )
+}
--- src/components/Shirts.js
+++ src/components/Shirts.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import Item from './Item'
+import englandShirt from '../images/england-shirt.jpeg'
 
 export default function Shirts() {
   return (
-    <div>Shirts</div>
+    <div className="products-grid">
+      <Item title="England Shirt" imgSrc={englandShirt} price="£60.00"></Item>
+      <Item title="England Shirt" imgSrc={englandShirt} price="£60.00"></Item>
+      <Item title="England Shirt" imgSrc={englandShirt} price="£60.00"></Item>
+      <Item title="England Shirt" imgSrc={englandShirt} price="£60.00"></Item>
+      <Item title="England Shirt" imgSrc={englandShirt} price="£60.00"></Item>
+      <Item title="England Shirt" imgSrc={englandShirt} price="£60.00"></Item>
+      <Item title="England Shirt" imgSrc={englandShirt} price="£60.00"></Item>
+      <Item title="England Shirt" imgSrc={englandShirt} price="£60.00"></Item>
+    </div>
   )
 }

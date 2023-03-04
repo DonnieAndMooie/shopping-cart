21409e4327059556df6899ff15f8dec13796ca0e
DonnieAndMooie <owenjeaves@gmail.com>
2023-03-04 10:52:18 +0000

    Create and style contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import Nav from './Nav'
+import map from '../images/map.avif'
+import twitter from '../images/icons8-twitter.svg'
+import facebook from '../images/facebook.svg'
+import instagram from '../images/instagram.svg'
 
 export default function Contact() {
   return (
-    <div>
+    <div className='contact'>
         <Nav></Nav>
-        Contact
+        <h1 className='contact-heading'>Contact Us</h1>
+        <img className='map' src={map} alt="Map" />
+        <p className='address'>Find us at:</p>
+        <p className='address'>18 Imaginary Road</p>
+        <p className='address'>Imaginary Town</p>
+        <p className='address'>Imaginary Country</p>
+        <p className='address'>MK23 DEI</p>
+        <div className="social-media">
+          <img className='social-media-icon' src={twitter} alt="Twitter" />
+          <h3 className='social-media-heading'>@FakeFootballStore</h3>
+          <img className='social-media-icon' src={facebook} alt="Facebook" />
+          <h3 className='social-media-heading'>FakeFootballOfficial</h3>
+          <img className='social-media-icon' src={instagram} alt="Instagram" />
+          <h3 className='social-media-heading'>@FakeFootballStoreOfficial</h3>
+        </div>
     </div>
   )
 }
